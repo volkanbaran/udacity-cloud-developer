564ddca7ed97eb0c46041e7f72cb07ecb700f291
volkanbaran <uaq4dc7g>
2019-09-15 20:23:34 +0300

    util fix for errors and filtered image implementation

--- project-2/image-filter-starter-code/src/server.ts
+++ project-2/image-filter-starter-code/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+
+
 
 (async () => {
 
@@ -9,38 +11,45 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
+
+  app.get("/filteredimage", async (req, res, next) => {
+
+    const imageUrl: string = req.query.image_url;
+
+    //validateUrl
+    if (!imageUrl) {
+      res.status(400).send("image_url is required")
+    }
+
+    //filter image
+    filterImageFromURL(imageUrl, next).then(filiteredImagePath => {
+      res.status(200).sendFile(filiteredImagePath, () => {
+        //delete local files after response
+        const files = [filiteredImagePath]
+        deleteLocalFiles(files);
+      })
+    }).catch(error => {
+      // filterImageFromURL throws exception if url is not image or valid
+      res.status(422).send(error.message);
+      console.log(error);
+    });
+
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
